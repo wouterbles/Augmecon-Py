9c46a6b918e5e5f8d0d75a67327716608ac5cc95
Wouter Bles <whbles@gmail.com>
2026-04-27 01:02:02 +0200

    Restructure as Python package

---
+++ augmecon_py/__init__.py
@@ -0,0 +1,5 @@
+"""Augmecon-Py reference implementation, packaged for installation."""
+
+from .augmecon import MoipAugmeconR, Solution
+
+__all__ = ["MoipAugmeconR", "Solution"]
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from src.augmecon import MoipAugmeconR
-from src.reader import read_excel_model
+from augmecon_py.augmecon import MoipAugmeconR
+from augmecon_py.reader import read_excel_model
 
 mod = read_excel_model("3kp40")
 A = MoipAugmeconR(mod)
